a9e7ba3df6f58d091df6ab3ec5ae49d12d3415af
Fikri Alwan Ramadhan <fikriar93@gmail.com>
2022-09-27 11:08:18 +0700

    feat: add content style and calculate function in square page

--- two-dimensional/square/index.js
+++ two-dimensional/square/index.js
@@ -1,1 +1,21 @@
-console.log("Square");
+const lengthOfSquare = document.querySelector("#length-of-square");
+const areaOfSquare = document.querySelector("#area");
+const perimeterOfSquare = document.querySelector("#perimeter");
+const formElement = document.querySelector('form');
+
+function calculatePerimeter(lengthOfSquare) {
+  return 4 * lengthOfSquare;
+}
+
+function calculateArea(lengthOfSquare) {
+  return lengthOfSquare ** 2;
+}
+
+formElement.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const resultCalculatePerimeter = calculatePerimeter(lengthOfSquare.value);
+  const resultCalculateArea = calculateArea(lengthOfSquare.value);
+
+  areaOfSquare.innerText = resultCalculateArea;
+  perimeterOfSquare.innerText = resultCalculatePerimeter;
+});
