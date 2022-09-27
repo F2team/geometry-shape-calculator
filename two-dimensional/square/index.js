f1ff19dfd78203cb9236ebc2cd312601d9f98f7f
M Haidar Hanif <git@mhaidarhanif.com>
2022-09-27 19:37:00 +0700

    feat: handle calculate square perimeter error conditions

--- two-dimensional/square/index.js
+++ two-dimensional/square/index.js
@@ -1,21 +1,45 @@
 const lengthOfSquare = document.querySelector("#length-of-square");
 const areaOfSquare = document.querySelector("#area");
 const perimeterOfSquare = document.querySelector("#perimeter");
-const formElement = document.querySelector('form');
+const formElement = document.querySelector("form");
 
-function calculatePerimeter(lengthOfSquare) {
-  return 4 * lengthOfSquare;
+function calculateSquarePerimeter(lengthOfSquare) {
+  try {
+    if (!lengthOfSquare) {
+      throw new Error("Length of square is required");
+    }
+    if (typeof lengthOfSquare !== "number") {
+      throw new Error("Length of square has to be a number");
+    }
+    return 4 * lengthOfSquare;
+  } catch (error) {
+    console.error(error);
+    alert(error.message);
+    return 0;
+  }
 }
 
 function calculateArea(lengthOfSquare) {
   return lengthOfSquare ** 2;
 }
 
-formElement.addEventListener('submit', (event) => {
+formElement.addEventListener("submit", (event) => {
   event.preventDefault();
-  const resultCalculatePerimeter = calculatePerimeter(lengthOfSquare.value);
-  const resultCalculateArea = calculateArea(lengthOfSquare.value);
 
-  areaOfSquare.innerText = resultCalculateArea;
-  perimeterOfSquare.innerText = resultCalculatePerimeter;
+  try {
+    const resultcalculateSquarePerimeter = calculateSquarePerimeter(
+      lengthOfSquare.value
+    );
+    const resultCalculateArea = calculateArea(lengthOfSquare.value);
+
+    if (!resultcalculateSquarePerimeter) {
+      throw new Error("Perimeter reseult is not available");
+    }
+
+    areaOfSquare.innerText = resultCalculateArea;
+    perimeterOfSquare.innerText = resultcalculateSquarePerimeter;
+  } catch (error) {
+    console.error(error);
+    alert("Perimeter reseult is not available");
+  }
 });
