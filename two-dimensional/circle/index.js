9f9a989f979ee6f0b3dca4f653ebf307f38ced22
mohammad farizan <mohammadfarizan224@gmail.com>
2022-09-27 18:24:51 +0700

    feat: edit index.js

--- two-dimensional/circle/index.js
+++ two-dimensional/circle/index.js
@@ -1,1 +1,23 @@
-console.log("Circle");
+const radiusOfCircumference = document.querySelector("#radius-of-circle");
+const areaOfCircle = document.querySelector("#area");
+const circumferenceOfCircle = document.querySelector("#circumference");
+const formElement = document.querySelector("form");
+
+function calculateCircumference(radiusOfCircumference) {
+  return 2 * 3.14 * radiusOfCircumference;
+}
+
+function calculateArea(radiusOfCircumference) {
+  return 3.14 * radiusOfCircumference ** 2;
+}
+
+formElement.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const resultCalculateCircumference = calculateCircumference(
+    radiusOfCircumference.value
+  );
+  const resultCalculateArea = calculateArea(radiusOfCircumference.value);
+
+  areaOfCircle.innerText = resultCalculateArea;
+  circumferenceOfCircle.innerText = resultCalculateCircumference;
+});
