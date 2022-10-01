99ca7669d1e5bb4d71d187f1b4e7d5a2b2c0503d
mohammad farizan <mohammadfarizan224@gmail.com>
2022-10-01 23:19:27 +0700

    feat: edit spher.js

---
+++ modules/sphere.js
@@ -0,0 +1,7 @@
+export function calculateVolumeSphere(sphereRadius) {
+  return (4 / 3) * Math.PI * sphereRadius ** 3;
+}
+
+export function calculateSurfaceAreaSphere(sphereRadius) {
+  return 4 * Math.PI * sphereRadius ** 2;
+}
